ee480b26398976c300952aa3420a27f3182c4e44
Albert Ramos <albertramos.dev@gmail.com>
2021-08-23 15:14:13 -0700

    initial commit

--- chp_2_variables/ex_3.rb
+++ chp_2_variables/ex_3.rb
@@ -1,1 +1,7 @@
 # Add another section onto name.rb that prints the name of the user 10 times. You must do this without explicitly writing the puts method 10 times in a row. Hint: you can use the times method to do something repeatedly.
+
+puts "Please tell me your name"
+name = gets.chomp
+10.times do |n|
+	puts "Hello " + name
+end
--- chp_2_variables/ex_5.rb
+++ chp_2_variables/ex_5.rb
@@ -15,3 +15,11 @@
 # puts x
 
 # What does x print to the screen in each case? Do they both give errors? Are the errors different? Why?
+
+# In the first case, 'puts x' prints 3.
+
+# In the second case, 'puts x' results in an error.
+
+# The first case works since 'x' was defined outside the times method block. 
+
+# The second case results in an error since 'x' was only defined within the times method block, therefore it results in an 'undefined local variable' error. Outer scope cannot access inner scope variables.
