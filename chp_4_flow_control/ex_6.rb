ee5d2fa8ac389dc68f9d1481ede59cd0569ee8ca
Albert Ramos <albertramos.dev@gmail.com>
2021-08-24 10:42:18 -0700

    initial commit

---
+++ chp_4_flow_control/ex_6.rb
@@ -0,0 +1,9 @@
+# Write down whether the following expressions return true or false or raise an error. Then, type the expressions into irb to see the results.
+
+(32 * 4) >= "129"
+847 == '874'
+'847' < '846'
+'847' > '846'
+'847' > '8478'
+'847' < '8478'
+
