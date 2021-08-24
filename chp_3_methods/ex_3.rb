0a1216cd264c6777c6ff1318de6447bd900e20c9
Albert Ramos <albertramos.dev@gmail.com>
2021-08-24 10:15:45 -0700

    initial commit

---
+++ chp_3_methods/ex_3.rb
@@ -0,0 +1,1 @@
+# Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
---
+++ chp_3_methods/ex_5.rb
@@ -0,0 +1,10 @@
+# 1) Edit the method definition in exercise #4 so that it does print words on the screen. 2) What does it return now?
+
+# Method from exercise 4:
+# def scream(words)
+#   words = words + "!!!!"
+#   return
+#   puts words
+# end
+
+# scream("Yippeee")
