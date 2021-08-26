b59da32c54c420596dc797b76b00302305b9ac57
Albert Ramos <albertramos.dev@gmail.com>
2021-08-26 04:07:49 -0700

    initial commit

--- chp_5_loops/ex_1.rb
+++ chp_5_loops/ex_1.rb
@@ -4,3 +4,5 @@ x = [1, 2, 3, 4, 5]
 x.each do |a|
   a + 1
 end
+
+#The each method simply returns the original array.
--- chp_5_loops/ex_2.rb
+++ chp_5_loops/ex_2.rb
@@ -1,2 +1,15 @@
 # Write a while loop that takes input from the user, performs an action, and only stops when the user types "STOP". Each loop can get info from the user.
 
+
+loop do
+  puts "Tell me a three letter word."
+  small_word = gets.chomp
+  puts "You said: #{small_word}. Neat."
+
+  puts "Do you want to do that again? If yes, enter any key. If no, enter STOP."
+  answer = gets.chomp
+  if answer == 'STOP'
+    break
+  end
+end
+
