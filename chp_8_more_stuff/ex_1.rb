8ed62814f18744a071f27c3862722abd089c1c12
Albert Ramos <albertramos.dev@gmail.com>
2021-09-22 14:47:32 -0700

    Exercise done

--- chp_8_more_stuff/ex_1.rb
+++ chp_8_more_stuff/ex_1.rb
@@ -1,7 +1,22 @@
-Write a program that checks if the sequence of characters "lab" exists in the following strings. If it does exist, print out the word.
+# Write a program that checks if the sequence of characters "lab" exists in the following strings. If it does exist, print out the word.
+#
+# "laboratory"
+# "experiment"
+# "Pans Labyrinth"
+# "elaborate"
+# "polar bear"
 
-"laboratory"
-"experiment"
-"Pans Labyrinth"
-"elaborate"
-"polar bear"
+def check_in(word)
+  if /lab/ =~ word
+    puts word
+  else
+    puts "No match"
+  end
+end
+
+
+check_in("laboratory")
+check_in("experiment")
+check_in("Pans Labyrinth")
+check_in("elaborate")
+check_in("polar bear")
