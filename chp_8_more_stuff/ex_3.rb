5ccf46d91c942e686ef23d1fc811fa8fed8b1eb0
Albert Ramos <albertramos.dev@gmail.com>
2021-09-22 14:46:40 -0700

    Exercise done

---
+++ chp_8_more_stuff/ex_3.rb
@@ -0,0 +1,8 @@
+# What is exception handling and what problem does it solve?
+
+# IIRC, exception handling is a way to check that code functions as intended. In Java, there are try & catch blocks inserted into code to "catch" certain errors or abberations.
+
+# Launch School answer:
+# Exception handling is a structure used to handle the possibility of an error occurring in a program. It is a way of handling the error by changing the flow of control without exiting the program entirely.
+
+# So I was missing the idea of changing flow of control in order to prevent exiting the program entirely.
