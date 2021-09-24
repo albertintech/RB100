8e8c42af80bf794252b9b1c2be377139f7d59913
Albert Ramos <albertramos.dev@gmail.com>
2021-09-24 07:51:11 -0700

    Exercise done

--- exercises/ex10.rb
+++ exercises/ex10.rb
@@ -1,1 +1,26 @@
 # Can hash values be arrays? Can you have an array of hashes? (give examples)
+
+# Yes and yes.
+
+# Example from geeks for geeks:
+
+hash_arr = [ {"height" => '5 ft', "weight" => '170 lbs',
+              "hair" => 'white'},
+             {:pet => 'Cat', :nest => 'Bird'} ]
+
+# Another method from geeks for geeks:
+
+hash_arr = []
+hash1 = {"height" => '5 ft', "weight" => '170 lbs', "hair" => 'White'}
+hash2 = {:pet => 'Frog', :nest => 'Bird'}
+
+hash_arr.push(hash1)
+hash_arr.push(hash2)
+
+# LS Examples
+
+# hash values as arrays
+hash = {names: ['bob', 'joe', 'susan']}
+
+# array of hashes
+arr = [{name: 'bob'}, {name: 'joe'}, {name: 'susan'}]
