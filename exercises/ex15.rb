a4c132cc9f159411dcd2274e36c658e46ed9f323
Albert Ramos <albertramos.dev@gmail.com>
2021-09-24 08:18:57 -0700

    Exercise done

--- exercises/ex15.rb
+++ exercises/ex15.rb
@@ -1,10 +1,14 @@
-What will the following program output?
+# What will the following program output?
 
 hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
 hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+
 
 if hash1 == hash2
   puts "These hashes are the same!"
 else
   puts "These hashes are not the same!"
 end
+
+# Results in "These hashes are the same". In Ruby, hash order is not a factor in determining equality.
