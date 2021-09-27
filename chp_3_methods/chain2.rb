385986a2e121a6e300add227b875f77894b793b8
Albert Ramos <albertramos.dev@gmail.com>
2021-09-27 15:45:43 -0700

    Add comments

--- chp_3_methods/chain2.rb
+++ chp_3_methods/chain2.rb
@@ -4,4 +4,4 @@ end
 
 add_three(5).times { puts "Will this work?" }
 
-# Use irb to def the method and call add_three(5) alone. It will show a return value of nil. Since that is the case, the chain breaks down and will not work.
+# Use irb to def the method and call add_three(5) alone. It will show a return value of nil. This is becuase puts always returns nil. Since that is the case, the chain breaks down and will not work.
---
+++ chp_3_methods/chain3.rb
@@ -0,0 +1,5 @@
+def add_three(n)
+  new_value = n + 3
+  puts new_value
+  new_value # Could use 'return' here, but works w/o since return is implied
+end
