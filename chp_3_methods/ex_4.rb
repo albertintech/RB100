79ccfd1cdd01c2cd6bd834e53dc0f1efdf5f4b6a
Albert Ramos <albertramos.dev@gmail.com>
2021-09-27 17:10:56 -0700

    Exercise done

--- chp_3_methods/ex_4.rb
+++ chp_3_methods/ex_4.rb
@@ -1,9 +1,11 @@
 # # What will the following code print to the screen?
 
-# def scream(words)
-#   words = words + "!!!!"
-#   return
-#   puts words
-# end
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
 
-# scream("Yippeee")
+scream("Yippeee")
+
+# this will output nothing to the screen
