a0dc969a89c5fc2173a35d083186484a0c02d519
Albert Ramos <albertramos.dev@gmail.com>
2021-09-27 17:16:32 -0700

    Exercise done

--- chp_3_methods/ex_6.rb
+++ chp_3_methods/ex_6.rb
@@ -1,7 +1,8 @@
 # What does the following error message tell you?
 
-# ArgumentError: wrong number of arguments (1 for 2)
-#   from (irb):1:in `calculate_product'
-#   from (irb):4
-#   from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
 
+  # That an argument is missing from a method call.
