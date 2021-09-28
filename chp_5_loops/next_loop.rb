e3f0b8e3c6ee15bbe855a1a8381cb12e7e2384ba
Albert Ramos <albertramos.dev@gmail.com>
2021-09-28 11:45:23 -0700

    Re-typed program

--- chp_5_loops/next_loop.rb
+++ chp_5_loops/next_loop.rb
@@ -1,11 +1,23 @@
+# i = 0
+# loop do
+# 	i = i + 2
+# 	if i == 4
+# 		next #skip the rest of the code in this iteration
+# 	end
+# 	puts i
+# 	if i == 10
+# 		break
+# 	end
+# end
+
 i = 0
-loop do 
+loop do
 	i = i + 2
 	if i == 4
-		next #skip the rest of the code in this iteration
+		next # skip the rest of the code inthis iteration
 	end
-	puts i 
+	puts i
 	if i == 10
-		break
+		break # exit loop after printing '10'
 	end
 end
