48a56957acc1493698bc1924bca808aaee6d5f5c
Albert Ramos <albertramos.dev@gmail.com>
2021-09-28 14:35:40 -0700

    Add capitalize method

--- chp_5_loops/perform_again.rb
+++ chp_5_loops/perform_again.rb
@@ -1,6 +1,6 @@
-loop do 
+loop do
 	puts "Do you want to do that again? If yes, press any key then enter. If you would like to stop, enter 'N'."
-	answer = gets.chomp
+	answer = gets.chomp.capitalize
 	if answer == 'N'
 		break
 	end
