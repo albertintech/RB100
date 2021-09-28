665587fed2a5d955704bc591ae90d0b70df2ce7d
Albert Ramos <albertramos.dev@gmail.com>
2021-09-28 13:44:17 -0700

    Re-type and amend program

--- chp_5_loops/countdown.rb
+++ chp_5_loops/countdown.rb
@@ -1,12 +1,27 @@
-puts "Give a starting integer to count down from: "
+# puts "Give a starting integer to count down from: "
+#
+# x = gets.chomp.to_i
+#
+# puts "Starting from #{x}...Go!"
+#
+# while x >= 0
+# 	puts x
+# 	x -= 1
+# end
+#
+# puts "Done!"
+
+puts "Give a starting integer to start the countdown: "
 
 x = gets.chomp.to_i
 
-puts "Starting from #{x}...Go!"
+puts "Thanks. Counting down from #{x}...Go!"
 
 while x >= 0
-	puts x 
+	puts x
 	x -= 1
 end
 
 puts "Done!"
+
+# Unlike a loop, while is not implementend as a method, so while does not have its own local scope. The entire body of the loop is in the same scope as the code that contains the while loop.
