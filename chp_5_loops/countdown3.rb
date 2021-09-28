8b2ed42b49cbdd6fbe5de9b2ea515b493e93d42a
Albert Ramos <albertramos.dev@gmail.com>
2021-09-28 14:44:43 -0700

    Re-type for loop

--- chp_5_loops/countdown3.rb
+++ chp_5_loops/countdown3.rb
@@ -3,8 +3,8 @@ x = gets.chomp.to_i
 puts "You have chosen #{x}."
 puts "Starting at #{x}, I will count down by one until I reach 0."
 
-for i in 0..x do 
-	puts x 
+for i in 0..x do
+	puts x
 	x -= 1
 end
 
