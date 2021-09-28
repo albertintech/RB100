ac394a4d571bbdc643b078033d596345d3bfaa89
Albert Ramos <albertramos.dev@gmail.com>
2021-09-27 17:06:16 -0700

    Exercise done

--- chp_3_methods/ex_2.rb
+++ chp_3_methods/ex_2.rb
@@ -1,12 +1,19 @@
 # What do the following expressions evaluate to?
 
-# 	1. x = 2
+# 1. x = 2
+
+# Variable assignment should evaluate to assigned value => 2
 
 # 2. puts x = 2
 
+# Outputs 2 and returns nil
+
 # 3. p name = "Joe"
 
-# 4. four = "four"
+# p will output "Joe" and return "Joe"
 
-# 5. print something = "nothing"
+# 4. four = "four" # => "four"
 
+# 5. print something = "nothing" # =>
+
+# print returns nil. So the output is nothing=>nil
