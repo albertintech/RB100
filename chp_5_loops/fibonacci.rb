d3a0201f188a77956164f127b2d9a3aa7e6ccb7a
Albert Ramos <albertramos.dev@gmail.com>
2021-09-29 15:29:01 -0700

    Re-type and expand program to accept user input

--- chp_5_loops/fibonacci.rb
+++ chp_5_loops/fibonacci.rb
@@ -1,9 +1,18 @@
+# With assistance from: https://www.includehelp.com/ruby/print-fibonacci-series.aspx
+
 def fibonacci(number)
 	if number < 2
-		number
+		return 1
 	else
-		fibonacci(number - 1) + fibonacci(number - 2)
+		return fibonacci(number - 1) + fibonacci(number - 2)
 	end
 end
 
-puts fibonacci(6)
+puts "Enter the number of terms: "
+number = gets.chomp.to_i
+
+puts "The first #{number} terms of the Fibonacci series are: "
+
+for i in 0..(number - 1)
+	puts fibonacci(i)
+end
